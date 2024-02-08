1e43950def9eaeb56910af4ccb46c0cabc6ec918
unknown <LENOVO S145@LENOVO-S145-14IWL-1TB-W10PRO.LENOVO-S145-14IWL-1TB-W10PRO>
2024-02-07 20:45:21 -0500

    proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Movil',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
